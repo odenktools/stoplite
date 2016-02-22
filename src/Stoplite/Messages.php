95cfc755f09fb184ff81a97f34a03532e7c72574
rahyan <rahyan.ramadhani@gmail.com>
2016-02-22 14:09:39 +0700

    Add Messages

---
+++ src/Stoplite/Messages.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Odenktools\Stoplite;
+
+class Messages
+{
+	const INVALID_CREDENTIALS = 'stoplite.invalid_credentials';
+	const UNVERIFIED = 'stoplite.unverified';
+	const DISABLED = 'stoplite.disabled';
+	const SUCCESS = 'stoplite.success';
+	const EXPIRED = 'stoplite.expired';
+}
